81df153df60c4e09993a51f4b8fbbd14eed62f10
Daniel Díaz <dalejodc@gmail.com>
2019-02-07 16:39:06 -0600

    Background added to the home component
    As /home is the default component, this commits has been to add the background and class active

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -10,6 +10,16 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.active();
   }
 
+  ngOnDestroy(): void {
+    var element = document.getElementById("home");
+    element.classList.remove("active");
+  }
+
+  active() {
+    var element = document.getElementById("home");
+    element.classList.add("active");
+  }
 }
