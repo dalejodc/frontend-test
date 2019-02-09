c7b045f99566a8c655f89326b1c48f3c247ecbde
Daniel Díaz <dalejodc@gmail.com>
2019-02-09 12:35:34 -0600

    Unnecessary lines eliminated
    After to try to use Slick slider

--- src/app/components/my-work/my-work.component.ts
+++ src/app/components/my-work/my-work.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as $ from 'jquery';
+
 @Component({
   selector: 'app-my-work',
   templateUrl: './my-work.component.html',
@@ -10,6 +12,6 @@ export class MyWorkComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    
   }
-
 }
