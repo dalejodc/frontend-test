71af962107ad86aa5b888d569c6bf3a08cf98e94
Daniel Díaz <dalejodc@gmail.com>
2019-02-09 20:14:58 -0600

    @HostListener
    Event to listen the windows size and don't display elements in the DOM

--- src/app/components/clients/clients.component.ts
+++ src/app/components/clients/clients.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HostListener } from '@angular/core'
+
 
 @Component({
   selector: 'app-clients',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientsComponent implements OnInit {
 
-  constructor() { }
+   constructor() { }
+  show: boolean = true;
 
   ngOnInit() {
   }
 
+  //Event to listen the windows size and don't display elements in the DOM
+  @HostListener('window:resize', ['$event'])
+  onResize(event){
+     console.log("Width: " + event.target.innerWidth);
+
+     //To hide an element
+     if(event.target.innerWidth <=766){
+      this.show = false;
+      console.log(this.show);
+    }else{
+      this.show = true;
+      console.log(this.show);
+     }
+
+  }
 }
