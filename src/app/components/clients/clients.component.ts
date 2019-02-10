3726c4e6e09ae0f7c5e7f02644cdb4bfe921d84b
Daniel Díaz <dalejodc@gmail.com>
2019-02-09 20:38:24 -0600

    Checking screen size onInit

--- src/app/components/clients/clients.component.ts
+++ src/app/components/clients/clients.component.ts
@@ -9,24 +9,36 @@ import { HostListener } from '@angular/core'
 })
 export class ClientsComponent implements OnInit {
 
-   constructor() { }
+   constructor() {}
+
   show: boolean = true;
+  public innerWidth: any;
 
   ngOnInit() {
+    this.checkSize();
+  }
+
+  checkSize(){
+    this.innerWidth = window.innerWidth;
+    if(this.innerWidth <=766){
+      this.show = false;
+      // console.log(this.show);
+    }else{
+      this.show = true;
+      // console.log(this.show);
+     }
   }
 
   //Event to listen the windows size and don't display elements in the DOM
-  @HostListener('window:resize', ['$event'])
-  onResize(event){
-     console.log("Width: " + event.target.innerWidth);
+  @HostListener('window:resize', ['$event']) onResize(event){
 
      //To hide an element
      if(event.target.innerWidth <=766){
       this.show = false;
-      console.log(this.show);
+      // console.log(this.show);
     }else{
       this.show = true;
-      console.log(this.show);
+      // console.log(this.show);
      }
 
   }
