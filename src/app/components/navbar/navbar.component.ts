91c49ae40d52a744994b36c845ef7e4c872ec5ff
Daniel Díaz <dalejodc@gmail.com>
2019-02-11 16:00:05 -0600

    Navbar style

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -8,34 +8,17 @@ import { Component, OnInit, HostListener } from '@angular/core';
 export class NavbarComponent implements OnInit {
 
   sticky: boolean = false;
-  
+
   constructor() { }
 
   ngOnInit() {
   }
-  //   // When the user scrolls the page, execute myFunction
-  //   window.onscroll = function () { myFunction() };
-
-  // // Get the navbar
-  // var navbar = document.getElementById("navbar");
-
-  // // Get the offset position of the navbar
-  // var sticky = navbar.offsetTop;
-
-  // // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-  // function myFunction() {
-  //   if (window.pageYOffset >= sticky) {
-  //     navbar.classList.add("sticky")
-  //   } else {
-  //     navbar.classList.remove("sticky");
-  //   }
-  // } 
 
   @HostListener('window:scroll', ['$event'])
   onWindowScroll($event) {
     let navbar = document.getElementById("navbar");
-  
-  let sticky = navbar.offsetTop;
+
+    let sticky = navbar.offsetTop;
     navbar.classList.add("sticky");
   }
 
