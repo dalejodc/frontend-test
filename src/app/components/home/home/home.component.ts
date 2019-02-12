e0bfaa176fb7bec3c20a786abf130eeb0c60903c
Daniel Díaz <dalejodc@gmail.com>
2019-02-12 12:24:05 -0600

    Navbar mobile active route

--- src/app/components/home/home/home.component.ts
+++ src/app/components/home/home/home.component.ts
@@ -1,29 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router: Router) { }
+    constructor(private router: Router) { }
 
-  ngOnInit() {
-    this.active();
-  }
-
-  // ngOnDestroy(): void {
-  //   var element = document.getElementById("home");
-  //   element.classList.remove("active");
-  // }
-
-  active() {
-    if (this.router.url === '/') {
-      var element = document.getElementById("home");
-      element.classList.add("active");
+    ngOnInit() {
+        this.active();
     }
 
-  }
+    // ngOnDestroy(): void {
+    //   var element = document.getElementById("home");
+    //   element.classList.remove("active");
+    // }
+
+    active() {
+        if (this.router.url === '/') {
+            var element = document.getElementById("home");
+            element.classList.add("active");
+        }
+    }
 }
--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -14,24 +14,26 @@ export class NavbarComponent implements OnInit {
     constructor(private router: Router) { }
 
     ngOnInit() {
+        
     }
 
     @HostListener('window:scroll', ['$event'])
     onWindowScroll($event) {
         let navbar = document.getElementById("navbar");
 
-
         if (window.scrollY > 73) {
             navbar.classList.add("sticky", "animated", "slideInDown");
         } else {
             navbar.classList.remove("sticky", "animated", "slideInDown");
         }
-   
+
     }
+
+  
 
 
     goTo(route: string) {
-        // To check if is the same route
+        // To check if it is the same route
         if (this.router.url === `/${route}`) {
             this.hide();
         } else {
@@ -41,10 +43,33 @@ export class NavbarComponent implements OnInit {
 
     show() {
         this.showMobileMenu = true;
+
+        this.checkCurrentRoute();
     }
 
     hide() {
         this.showMobileMenu = false;
     }
 
+    checkCurrentRoute() {
+
+        // Getting the current url
+        let url: string = this.router.url;
+
+        // Delenting the char '/' of the route
+        let optionMenu = url.slice(1, url.length);
+
+        // To check default '/' route
+        if (url.length > 1) {
+
+            // Giving time to wait the element
+            setTimeout(() => {
+
+                let menu = document.getElementById(`${optionMenu}-mobile`);
+
+                menu.classList.add("active-mobile", "animated", "fadeIn");
+
+            }, 100);
+        }
+    }
 }
