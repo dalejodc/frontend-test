73d5a6a59a7f9189b0f377dee337242de9422e9a
Daniel Díaz <dalejodc@gmail.com>
2019-02-11 18:23:10 -0600

    Newsletter form

--- src/app/components/newsletter/newsletter.component.ts
+++ src/app/components/newsletter/newsletter.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-newsletter',
   templateUrl: './newsletter.component.html',
   styleUrls: ['./newsletter.component.css']
 })
+
 export class NewsletterComponent implements OnInit {
+
+  message:boolean=false;
+
+  user: Object={
+		name: null,
+		username: null,
+		email:null,
+		country: ""
+	}
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  send(){
+    this.message = true;
+    this.user = '';
   }
 
 }
