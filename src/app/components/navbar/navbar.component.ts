47d52bbb2bc034ca54041551ab77c8fc526ae85b
Daniel Díaz <dalejodc@gmail.com>
2019-02-11 20:37:17 -0600

    Navbar issues fixed

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,34 +1,45 @@
 import { Component, OnInit, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
 
-  sticky: boolean = false;
-  showMobileMenu:boolean = false;
+    sticky: boolean = false;
+    showMobileMenu: boolean = false;
 
-  constructor() { }
+    constructor(private router: Router) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
-  @HostListener('window:scroll', ['$event'])
-  onWindowScroll($event) {
-    let navbar = document.getElementById("navbar");
+    @HostListener('window:scroll', ['$event'])
+    onWindowScroll($event) {
+        let navbar = document.getElementById("navbar");
 
-    let sticky = navbar.offsetTop;
-    navbar.classList.add("sticky");
-  }
+        let sticky = navbar.offsetTop;
+        navbar.classList.add("sticky");
+    }
 
-  show(){
-      this.showMobileMenu = true;
-  }
-  
-  hide(){
-      this.showMobileMenu =false;
-  }
+    
+    goTo(route: string) {
+        // To check if is the same route
+        if (this.router.url === `/${route}`) {
+            this.hide();
+        } else {
+            this.router.navigateByUrl(`/${route}`);
+        }
+    }
+
+    show() {
+        this.showMobileMenu = true;
+    }
+
+    hide() {
+        this.showMobileMenu = false;
+    }
 
 }
