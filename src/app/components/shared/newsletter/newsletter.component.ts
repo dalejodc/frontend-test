ab31543866376ecedf6d88815c38b9aabff280cd
Daniel Díaz <dalejodc@gmail.com>
2019-02-11 22:50:33 -0600

    Disabled style

--- src/app/components/shared/newsletter/newsletter.component.ts
+++ src/app/components/shared/newsletter/newsletter.component.ts
@@ -2,31 +2,34 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
-  selector: 'app-newsletter',
-  templateUrl: './newsletter.component.html',
-  styleUrls: ['./newsletter.component.css']
+    selector: 'app-newsletter',
+    templateUrl: './newsletter.component.html',
+    styleUrls: ['./newsletter.component.css']
 })
 
 export class NewsletterComponent implements OnInit {
 
-  message:boolean=false;
+    message: boolean = false;
+    error: boolean = false;
 
-  user: Object={
-		name: null,
-		username: null,
-		email:null,
-		country: ""
-	}
+    user: Object = {
+        name: null,
+        username: null,
+        email: null,
+        country: ""
+    }
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
-  send(){
-    console.log(this.user);
-    this.message = true;
-    this.user = '';
-  }
+    send() {
+        
+        console.log(this.user);
+        console.log(this.user.name);
+        // this.message = true;
+        this.user.name = '';
+    }
 
 }
