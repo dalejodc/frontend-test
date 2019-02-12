2e98e5afcc5ca6a6d0e43efe5e359093dda33154
Daniel Díaz <dalejodc@gmail.com>
2019-02-11 22:04:17 -0600

    New Scaffolding

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {HomeComponent} from './components/home/home.component';
+import {HomeComponent} from './components/home/home/home.component';
 import {ContactComponent} from './components/contact/contact.component';
 import {AboutComponent} from './components/about/about.component';
 
